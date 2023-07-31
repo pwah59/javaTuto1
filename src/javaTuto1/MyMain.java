146ac02ba562e4a5cf448e0a133f19b6abd47eb7
Patrick H <patrick.harle59@gmail.com>
2023-07-31 15:07:13 +0200

    first class MyMain

---
+++ src/javaTuto1/MyMain.java
@@ -0,0 +1,20 @@
+package javaTuto1;
+
+public class MyMain {
+	
+	public static void affiche(String s) {
+		// TODO Auto-generated method stub
+		System.out.println(s);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Hello Patrick, welcome on Eclipse IDE !!";
+		affiche(str);
+		str += " First sample is OK wthout bug : nice job !!";
+		affiche(str);
+	}
+	
+}
+	
+
